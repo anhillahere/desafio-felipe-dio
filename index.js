d8204d6ca852ac3aa9ec0b489cb6c3c8ae8c47e5
anhillahere <anhillahere@gmail.com>
2024-02-27 18:26:34 -0300

    tentando fazer seletivos

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//first step
+let nickname = "Pisa Fundo"
+
+let xpDiario = 100
+let xpAtual = 6400
+let xpTotal = xpAtual+=xpDiario
+
+console.log("Olá "+ nickname + ", seja bem vindo!")
+console.log("" + "+" + xpDiario + " exp adquirido")
+
+let rankBronze = 10001
+let rankPrata = 2001
+let rankOuro = 5001
+let rankPlat = 7001
+let rankAsc = 8001
+let rankImr = 9001
+let rankRad = 10001
+
+switch (xpTotal) {
+    case xpTotal<rankPrata:
+        console.log("Você avançou para Bronze")
+        break;
+}
